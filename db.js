de4b2fd95d01a06e535c83060ce5725c14024566
wangz282-1838578 <wangz282@uw.edu>
2022-03-06 22:28:32 -0800

    remote db created

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ let db = {};
 //    brew services start mongodb-community@5.0
 // TODO: change to online db
 async function dbConnect() {
-  await mongoose.connect("mongodb://localhost:27017/uwbuilding");
+  await mongoose.connect("mongodb+srv://info441projectUser:info441project@cluster0.y5q08.mongodb.net/info441GroupProject?retryWrites=true&w=majority");
   console.log("connected to the database");
 
   const buildingSchema = new mongoose.Schema({
